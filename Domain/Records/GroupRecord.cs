d2428851ca446ce99c8c4fb11417dbada70ccd45
Jan Fegeš <jan.fegesh@gmail.com>
2023-06-04 21:04:14 +0200

    Created some demo records.

---
+++ Domain/Records/GroupRecord.cs
@@ -0,0 +1,4 @@
+﻿namespace Domain.Records
+{
+    public record GroupRecord(int Id, string Name);
+}
---
+++ Domain/Records/UserRecord.cs
@@ -0,0 +1,4 @@
+﻿namespace Domain.Records
+{
+    public record UserRecord(int Id, string Name, string Surname, string Email, string PhoneNumber, string TaxNumber, GroupRecord? Group);
+}
